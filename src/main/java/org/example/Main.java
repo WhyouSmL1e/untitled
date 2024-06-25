889b42722c347f88b059dd49bd903fae3c1c9944
lc2637406787 <2637406787@qq.com>
2024-06-25 11:33:55 +0800

    lc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello dad word");
+        System.out.printf("Hello  word");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-        System.out.printf("Hello word");
-        System.out.printf("Hello word111");
+        System.out.printf("Helloadsadasdasd word");
     }
 }
